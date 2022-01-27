5677ed838515a572660170504852393e6c04faf9
Mary Martinez <mary.diana.martinez@gmail.com>
2022-01-27 15:12:22 -0800

    Add createCountString and tests and import functions + grab DOM elements

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // import functions and grab DOM elements
+import { createCountString } from './test/utils.js';
+const climateDropdown = document.getElementById('climate-dropdown');
+const archDropdown = document.getElementById('arch-dropdown');
+const mayorDropdown = document.getElementById('mayor-dropdown');
+const climateEl = document.getElementById('climate-image');
+const archEl = document.getElementById('arch-image');
+const mayorEl = document.getElementById('mayor-image');
+const sloganButton = document.getElementById('slogan-button');
+const sloganInput = document.getElementById('slogan-input');
+const sloganEl = document.getElementById('slogan-location');
 
 // let state
+let climateCounter = 0;
+let archCounter = 0;
+let mayorCounter = 0;
 
+let slogans =[''];
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+climateDropdown.addEventListener('change', (e) => {
+  const value = e.target.value;
+})
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,11 @@
 import './example.test.js';
+import { createCountString } from './utils.js';
+
+const { test: myTest, skip } = QUnit;
+
+myTest('this test should display the type and count of changes for the dropdown options', (expect) => {
+    const expected = 'You have changed the architecture option 3 times.'
+
+    const actual = createCountString('architecture', 3);
+    expect.equal(actual, expected);
+})
