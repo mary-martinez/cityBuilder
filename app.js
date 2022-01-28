d5e4c25cc1dd89a712729083a4fdf829f787315c
Mary Martinez <mary.diana.martinez@gmail.com>
2022-01-27 21:32:42 -0800

    more eslint fixes

--- app.js
+++ app.js
@@ -25,67 +25,67 @@ let slogans = [''];
 // update DOM to reflect the new state
 
 climateDropdown.addEventListener('change', (e) => {
-	//get value selected by user
-	const value = e.target.value;
-	console.log(value);
-	// increment climate counter
-	climateCounter++;
+    //get value selected by user
+    const value = e.target.value;
+    console.log(value);
+    // increment climate counter
+    climateCounter++;
 
-	// update image
-	climateEl.src = `./assets/${value}.jpg`;
-	// resultsEl.textContent = createCountString('climate', climateCounter);
-	// ** Need to call something here to display stats
-	displayStats();
+    // update image
+    climateEl.src = `./assets/${value}.jpg`;
+    // resultsEl.textContent = createCountString('climate', climateCounter);
+    // ** Need to call something here to display stats
+    displayStats();
 });
 
 archDropdown.addEventListener('change', (e) => {
-	//get value selected by user
-	const value = e.target.value;
-	console.log(value);
-	// increment climate counter
-	archCounter++;
+    //get value selected by user
+    const value = e.target.value;
+    console.log(value);
+    // increment climate counter
+    archCounter++;
 
-	// update image
-	archEl.src = `./assets/${value}.jpg`;
-	// resultsEl.textContent = createCountString('architecture', archCounter);
-	// ** Need to call something here to display stats
-	displayStats();
+    // update image
+    archEl.src = `./assets/${value}.jpg`;
+    // resultsEl.textContent = createCountString('architecture', archCounter);
+    // ** Need to call something here to display stats
+    displayStats();
 
 });
 
 mayorDropdown.addEventListener('change', (e) => {
-	//get value selected by user
-	const value = e.target.value;
-	console.log(value);
-	// increment climate counter
-	mayorCounter++;
+    //get value selected by user
+    const value = e.target.value;
+    console.log(value);
+    // increment climate counter
+    mayorCounter++;
 
-	// update image
-	mayorEl.src = `./assets/${value}.jpg`;
-	// resultsEl.textContent = createCountString('mayor', mayorCounter);
-	// ** Need to call something here to display stats
-	displayStats();
+    // update image
+    mayorEl.src = `./assets/${value}.jpg`;
+    // resultsEl.textContent = createCountString('mayor', mayorCounter);
+    // ** Need to call something here to display stats
+    displayStats();
 
 });
 
 sloganButton.addEventListener('click', () => {
-	slogans.push(sloganInput.value);
-	sloganInput.value = '';
-	displaySlogans();
+    slogans.push(sloganInput.value);
+    sloganInput.value = '';
+    displaySlogans();
 })
 
 function displayStats(type, count) {
-	resultsClimateEl.textContent = createCountString('climate', climateCounter);
-	resultsArchEl.textContent = createCountString('architecture', archCounter);
-	resultsMayorEl.textContent = createCountString('Mayor', mayorCounter);
+    resultsClimateEl.textContent = createCountString('climate', climateCounter);
+    resultsArchEl.textContent = createCountString('architecture', archCounter);
+    resultsMayorEl.textContent = createCountString('Mayor', mayorCounter);
 }
 
 function displaySlogans() {
-	sloganEl.textContent = '';
-	// console.log(slogans);
-	for (let slogan of slogans) {
-		const p = document.createElement('p');
-		p.textContent = slogan;
-		sloganEl.append(p);
-	}
+    sloganEl.textContent = '';
+    // console.log(slogans);
+    for (let slogan of slogans) {
+        const p = document.createElement('p');
+        p.textContent = slogan;
+        sloganEl.append(p);
+    }
 }
