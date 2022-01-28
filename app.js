555f5248d05af175280adcb5118ce8fa6cb250fc
Mary Martinez <mary.diana.martinez@gmail.com>
2022-01-27 16:29:25 -0800

    displayStats function and slogan event listener + displaySlogans function

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const mayorEl = document.getElementById('mayor-image');
 const sloganButton = document.getElementById('slogan-button');
 const sloganInput = document.getElementById('slogan-input');
 const sloganEl = document.getElementById('slogan-location');
-const resultsEl = document.getElementById('results');
+const resultsClimateEl = document.getElementById('resultsClimate');
+const resultsArchEl = document.getElementById('resultsArch');
+const resultsMayorEl = document.getElementById('resultsMayor');
 
 // let state
 let climateCounter = 0;
@@ -31,5 +33,59 @@ climateDropdown.addEventListener('change', (e) => {
 
   // update image
   climateEl.src = `./assets/${value}.jpg`;
-  resultsEl.textContent = createCountString('climate', climateCounter);
+  // resultsEl.textContent = createCountString('climate', climateCounter);
+  // ** Need to call something here to display stats
+  displayStats();
+});
+
+archDropdown.addEventListener('change', (e) => {
+  //get value selected by user
+  const value = e.target.value;
+  console.log(value);
+  // increment climate counter
+  archCounter++;
+
+  // update image
+  archEl.src = `./assets/${value}.jpg`;
+  // resultsEl.textContent = createCountString('architecture', archCounter);
+  // ** Need to call something here to display stats
+  displayStats();
+
+});
+
+mayorDropdown.addEventListener('change', (e) => {
+  //get value selected by user
+  const value = e.target.value;
+  console.log(value);
+  // increment climate counter
+  mayorCounter++;
+
+  // update image
+  mayorEl.src = `./assets/${value}.jpg`;
+  // resultsEl.textContent = createCountString('mayor', mayorCounter);
+  // ** Need to call something here to display stats
+  displayStats();
+
+});
+
+sloganButton.addEventListener('click', () => {
+    slogans.push(sloganInput.value);
+    sloganInput.value = '';
+    displaySlogans();
 })
+
+function displayStats(type, count){
+  resultsClimateEl.textContent = createCountString('climate', climateCounter);
+  resultsArchEl.textContent = createCountString('architecture', archCounter);
+  resultsMayorEl.textContent = createCountString('Mayor', mayorCounter);
+}
+
+function displaySlogans(){
+    sloganEl.textContent = '';
+    // console.log(slogans);
+    for (let slogan of slogans){
+        const p = document.createElement('p');
+        p.textContent = slogan;
+        sloganEl.append(p);
+    }
+}
